5cf354e100f12e09d55a02b16463d3877fd1044b
Julio <juliomourajom@gmail.com>
2019-04-23 16:46:56 -0300

    Segundo commit

--- src/main/kotlin/com/example/testeummilhao/ummilhao/models/Evento.kt
+++ src/main/kotlin/com/example/testeummilhao/ummilhao/models/Evento.kt
@@ -1,26 +1,17 @@
 package com.example.testeummilhao.ummilhao.models
 
-import java.io.Serializable
+import org.springframework.data.annotation.PersistenceConstructor
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-class Evento:Serializable{
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    val codigo: Long = 0
-
-    val nome: String? = null
-    val local: String? = null
-    val data: String? = null
-    val horario: String? = null
-
-    companion object{
-        private const val serialVersionUID = 1L
-    }
-
-
-}
+data class Evento @PersistenceConstructor constructor(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        val codigo: Long = 0,
+        val nome: String? = null,
+        val local: String? = null,
+        val data: String? = null,
+        val horario: String? = null)
