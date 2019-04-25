f54444284dd34334b4401c6e3c533c018f3433bf
Julio <juliomourajom@gmail.com>
2019-04-25 16:21:21 -0300

    Listando eventos na pagina /eventos

--- src/main/kotlin/com/example/testeummilhao/ummilhao/controllers/EventoController.kt
+++ src/main/kotlin/com/example/testeummilhao/ummilhao/controllers/EventoController.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.servlet.ModelAndView
 
 
 @Controller
@@ -28,5 +29,13 @@ class EventoController {
 
         return "redirect:/cadastrarEvento"
 
+    }
+
+    @RequestMapping("/eventos")
+    fun listEventos():ModelAndView{
+        val mv = ModelAndView("index")
+        val eventos = er?.findAll()
+        mv.addObject("eventos", eventos)
+        return mv
     }
 }
