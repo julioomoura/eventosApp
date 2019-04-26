0dfcd816fb6fa60b24f1e69ab8c9194ecc3fe99b
Julio <juliomourajom@gmail.com>
2019-04-26 13:23:54 -0300

    Adicionando link de evento

--- src/main/kotlin/com/example/testeummilhao/ummilhao/controllers/EventoController.kt
+++ src/main/kotlin/com/example/testeummilhao/ummilhao/controllers/EventoController.kt
@@ -5,7 +5,9 @@ import com.example.testeummilhao.ummilhao.repository.EventoRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
 
 
@@ -36,6 +38,15 @@ class EventoController {
         val mv = ModelAndView("index")
         val eventos = er?.findAll()
         mv.addObject("eventos", eventos)
+        return mv
+    }
+
+    @RequestMapping("/{codigo}")
+    fun detalhesEvento (@PathVariable("codigo") codigo: Long):ModelAndView{
+        var evento: Evento? = er?.findByCodigo(codigo)
+        val mv = ModelAndView("detalhesEvento")
+        mv.addObject("evento", evento)
+        println("evento $evento")
         return mv
     }
 }
--- src/main/kotlin/com/example/testeummilhao/ummilhao/repository/EventoRepository.kt
+++ src/main/kotlin/com/example/testeummilhao/ummilhao/repository/EventoRepository.kt
@@ -3,4 +3,8 @@ package com.example.testeummilhao.ummilhao.repository
 import com.example.testeummilhao.ummilhao.models.Evento
 import org.springframework.data.repository.CrudRepository
 
-interface EventoRepository : CrudRepository<Evento, String>
+interface EventoRepository : CrudRepository<Evento, String>{
+
+    fun findByCodigo(codigo: Long):Evento
+
+}
