4c4d625b51ce1dbdae89aa81d3bb6276ab7e959c
Júlio Moura <juliomourajom@gmail.com>
2019-05-10 01:36:27 -0300

    correções

--- src/main/kotlin/com/example/testeummilhao/ummilhao/repository/ConvidadoRepository.kt
+++ src/main/kotlin/com/example/testeummilhao/ummilhao/repository/ConvidadoRepository.kt
@@ -7,5 +7,4 @@ import org.springframework.data.repository.CrudRepository
 interface ConvidadoRepository:CrudRepository<Convidado, String>{
 
     fun findByEvento(evento: Evento?):Iterable<Convidado>
-    fun findByRg(rg:String):Convidado
 }
